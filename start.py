7800726500baf56a4a09874bd012ed4f470ebe2a
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-24 16:00:30 +0530

    K

---
+++ start.py
@@ -0,0 +1,4 @@
+from os import system # chala ja yaar
+system ("git clone -b new https://github.com/ULTRA-OP/ULTRA-X.git && cd ULTRA-X && python3 -m ULTRA")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
