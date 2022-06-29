cd1677e556054c76bf4a76d83deb2d6f1e477150
Brutal Rajput <93695693+opbrutal@users.noreply.github.com>
2022-06-29 11:09:39 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja yaar
-system ("git clone -b new https://github.com/ULTRA-OP/ULTRA-X.git && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone -b new https://github.com/opbrutal/ULTRA && cd ULTRA-X && python3 -m ULTRA")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @S4SAHILHACKER")
